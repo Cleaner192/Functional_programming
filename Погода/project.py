4aeb92edf6e468b6d473d81b02703100b2f13e80
Cleaner192 <117023024+Cleaner192@users.noreply.github.com>
2023-01-23 09:41:28 +0600

    Add files via upload

---
+++ Погода/project.py
@@ -0,0 +1,15 @@
+#коммент
+name = 'Біләл Азамат'
+a = int(input())
+if a <= 13:
+    print('вы мальенкий')
+elif a > 13 and a <= 24:
+    print('вы молодость')
+elif a > 24 and a <= 59:
+    print('вы взрослый')
+elif a > 59:
+    print('вы старый')
+
+
+
+    
