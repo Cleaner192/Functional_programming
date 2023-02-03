f7fb2da3dff535a3d3afa2d9f9b10d2147280133
Cleaner192 <117023024+Cleaner192@users.noreply.github.com>
2023-02-03 21:00:45 +0600

    Add files via upload

---
+++ Functional programming/lab3.py
@@ -0,0 +1,47 @@
+#################   1 задание    ##################  
+# print('Ваше первое значение')
+# a = int(input()) 
+# print('Ваше второе значение')
+# b = int(input()) 
+
+# while a<=b:
+#     print('Значение:',a)
+#     a=a+1
+
+
+#################   2 задание    ##################  
+# print('Ваше первое значение')
+# A = int(input())
+# print('Ваше второе значение')
+# B = int(input() )
+
+# if A<B:                             #A<B выводим по возрастанию
+#     for i in range(A, B+1):
+#         print('Вывод ',i)
+# else:                               #B<A по убыванию
+#     for i in reversed(range(B, A+1)):
+#         print('Вывод ',i)
+
+#################   3 задание    ##################  
+# print('Ваше первое значение')
+# a = int(input())
+# print('Ваше второе значение')
+# b = int(input())
+# for i in range(a-int((a%2)==0), b-1, -2):
+#    print('Вывод ',i)
+
+#################   4 задание    ##################  
+# N = int(input('Введите номер: '))
+# n_1 = int(input('Выбирайте карту: '))
+
+# for number in range(1, n_1):
+#     count = 0
+# count += 1
+# print(count, end=' ')
+# for number in range(n_1 +1, N +1):
+#     count1 = n_1
+# count1 += 1
+# print(count1, end=' ')
+# print('Номер потерянной карточки:', n_1)
+
+
